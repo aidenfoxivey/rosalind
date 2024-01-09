5645244b655ab57658c54382fe1c240dfb1efce4
Aiden Fox Ivey <aiden@fox-ivey.com>
2024-01-09 18:32:08 -0500

    formatted code

--- complementing_a_strand_of_dna.go
+++ complementing_a_strand_of_dna.go
@@ -7,15 +7,15 @@ import (
 func main() {
 	var s string
 	m := map[rune]rune{
-		'A':'T',
-		'T':'A',
-		'C':'G',
-		'G':'C',
+		'A': 'T',
+		'T': 'A',
+		'C': 'G',
+		'G': 'C',
 	}
 	fmt.Scanf("%s", &s)
 	rs := []rune(s)
 
-	for i := len(rs)-1; i >= 0; i-- {
+	for i := len(rs) - 1; i >= 0; i-- {
 		fmt.Printf("%c", m[rs[i]])
 	}
 
